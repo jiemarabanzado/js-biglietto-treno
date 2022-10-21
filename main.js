084d4b5dc770013c12c72318f4124494dbe76d2b
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-21 10:04:31 +0200

    controllo numero negativo

--- main.js
+++ main.js
@@ -14,24 +14,28 @@ var test=0.00;
 sessionStorage.setItem('tot',test)
 function Calcola(){
     document.getElementById('Distance').readOnly=true;
-    var Ages=GetAge();
-    var Km=GetDistance();
-    var Name=GetName();
+    var Ages=Number(GetAge());
+    var Km=Number(GetDistance());
+    var Name=(GetName());
     const fisso= (Km*0.21);
     var SingleP = fisso;
     let element = document.createElement("P");
-    if(Ages<18){
-        SingleP= fisso - (fisso/100*20).toFixed(2);
-        element.innerHTML = `${Name} <span>${fisso}$</span> ${SingleP}$`;
-    }else if(Ages>65){
-        SingleP= fisso - (fisso/100*40).toFixed(2);
-        element.innerHTML = `${Name} <span>${fisso}$</span> ${SingleP}$`;
-    }else{element.innerHTML = `${Name} ${SingleP}$`;}
-    document.getElementById('Age').value=null;
-    document.getElementById('Name').value=null;
-    document.getElementById("passeggeri").append(element);
-    var sum=(parseFloat(sessionStorage.getItem('tot'))+SingleP);
-    sessionStorage.setItem('tot',sum);
+    if(Ages<0){
+        alert('inserisci valore maggiore di zero')
+        return;;
+    }
+        else if(Ages<18){
+            SingleP= fisso - (fisso/100*20).toFixed(2);
+            element.innerHTML = `${Name} <span>${fisso}$</span> ${SingleP}$`;
+            }else if(Ages>65){
+            SingleP= fisso - (fisso/100*40).toFixed(2);
+            element.innerHTML = `${Name} <span>${fisso}$</span> ${SingleP}$`;
+            }else{element.innerHTML = `${Name} ${SingleP}$`;}
+            document.getElementById('Age').value=null;
+            document.getElementById('Name').value=null;
+            document.getElementById("passeggeri").append(element);
+            var sum=(parseFloat(sessionStorage.getItem('tot'))+SingleP);
+            sessionStorage.setItem('tot',sum);
 }
 function GetTotal(){
     var tot=(parseFloat(sessionStorage.getItem('tot')));
