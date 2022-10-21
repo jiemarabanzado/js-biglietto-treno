6b2958e3d00a79ed2eda662765dcdc3b6569e0c6
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-21 10:22:46 +0200

    controllo numero età

--- main.js
+++ main.js
@@ -14,28 +14,36 @@ var test=0.00;
 sessionStorage.setItem('tot',test)
 function Calcola(){
     document.getElementById('Distance').readOnly=true;
-    var Ages=Number(GetAge());
+    var Ages=GetAge();
     var Km=Number(GetDistance());
     var Name=(GetName());
     const fisso= (Km*0.21);
     var SingleP = fisso;
     let element = document.createElement("P");
-    if(Ages<0){
-        alert('inserisci valore maggiore di zero')
-        return;;
+    if(isNaN(Ages)){
+        alert('inserisci un numero')
+        return;
     }
-        else if(Ages<18){
-            SingleP= fisso - (fisso/100*20).toFixed(2);
-            element.innerHTML = `${Name} <span>${fisso}$</span> ${SingleP}$`;
-            }else if(Ages>65){
-            SingleP= fisso - (fisso/100*40).toFixed(2);
-            element.innerHTML = `${Name} <span>${fisso}$</span> ${SingleP}$`;
-            }else{element.innerHTML = `${Name} ${SingleP}$`;}
-            document.getElementById('Age').value=null;
-            document.getElementById('Name').value=null;
-            document.getElementById("passeggeri").append(element);
-            var sum=(parseFloat(sessionStorage.getItem('tot'))+SingleP);
-            sessionStorage.setItem('tot',sum);
+    else
+        {
+            Ages=Number(Ages);
+            if(Ages<0){
+            alert('inserisci valore maggiore di zero')
+            return;
+            }
+            else if(Ages<18){
+                SingleP= fisso - (fisso/100*20).toFixed(2);
+                element.innerHTML = `${Name} <span>${fisso}$</span> ${SingleP}$`;
+                }else if(Ages>65){
+                SingleP= fisso - (fisso/100*40).toFixed(2);
+                element.innerHTML = `${Name} <span>${fisso}$</span> ${SingleP}$`;
+                }else{element.innerHTML = `${Name} ${SingleP}$`;}
+                document.getElementById('Age').value=null;
+                document.getElementById('Name').value=null;
+                document.getElementById("passeggeri").append(element);
+                var sum=(parseFloat(sessionStorage.getItem('tot'))+SingleP);
+                sessionStorage.setItem('tot',sum);
+            }
 }
 function GetTotal(){
     document.getElementById('Name').readOnly=true;
