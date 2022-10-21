f206774bebf00d334176ffbe74518b44036f2ad6
jiemarabanzado <jiemar.abanzado@yahoo.it>
2022-10-21 09:11:18 +0200

    fix calcoli aggiunta session store

--- main.js
+++ main.js
@@ -10,11 +10,13 @@ function GetDistance() {
     let Km = document.getElementById("Distance").value;
     return Km;
 }
+var test=0.00;
+sessionStorage.setItem('tot',test)
 function Calcola(){
     var Ages=GetAge();
     var Km=GetDistance();
     var Name=GetName();
-    const fisso= Km*0.21;
+    const fisso= (Km*0.21);
     var SingleP = fisso;
     let element = document.createElement("P");
     if(Ages<18){
@@ -27,4 +29,14 @@ function Calcola(){
     document.getElementById('Age').value=null;
     document.getElementById('Name').value=null;
     document.getElementById("passeggeri").append(element);
+    var sum=(parseFloat(sessionStorage.getItem('tot'))+SingleP);
+    sessionStorage.setItem('tot',sum);
+}
+function GetTotal(){
+    var tot=(parseFloat(sessionStorage.getItem('tot')));
+    tot=tot.toFixed(2);
+    document.getElementById('total').innerHTML=`${tot}$`;
+    var element = document.getElementById("action");
+    element.style.display = "none";
+
 }
